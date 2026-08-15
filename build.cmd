887760ae02b1bee9ecb2c3f7123b425a561a2744
unknown <Alon.Reich@Gmail.com>
2026-08-15 15:32:54 +0300

    Fix build.cmd NativeAOT toolchain detection syntax

--- build.cmd
+++ build.cmd
@@ -190,7 +190,7 @@ if not defined VSWHERE (
   echo ERROR: Native AOT platform linker not found.
   exit /b 1
 )
-for /f "usebackq delims=" %%I in ("%VSWHERE%" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath 2^>nul) do (
+for /f "usebackq delims=" %%I in (`"%VSWHERE%" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath 2^>nul`) do (
   if exist "%%I\Common7\Tools\VsDevCmd.bat" (
     set "VSCMD_SKIP_SENDTELEMETRY=1"
     call "%%I\Common7\Tools\VsDevCmd.bat" -arch=x64 -host_arch=x64 >nul

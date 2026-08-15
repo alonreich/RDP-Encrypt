41a3fd6b358c060a6b439e6d5b5efdf5cd10ec36
unknown <Alon.Reich@Gmail.com>
2026-08-15 13:30:03 +0300

    Update installers to register in appwiz.cpl

--- installer.cmd
+++ installer.cmd
@@ -1,6 +1,24 @@
 @echo off
-echo Installing RDP Vault...
-mkdir "%USERPROFILE%\Desktop\RDP Vault" 2>nul
-copy /y ".\compiled\RDPVault.exe" "%USERPROFILE%\Desktop\RDP Vault\RDPVault.exe"
-echo Installed to Desktop\RDP Vault.
+setlocal
+echo =========================================
+echo  INSTALLING RDP VAULT
+echo =========================================
+
+set "INSTALL_DIR=%LOCALAPPDATA%\RDPVault"
+mkdir "%INSTALL_DIR%" 2>nul
+copy /y ".\compiled\RDPVault.exe" "%INSTALL_DIR%\RDPVault.exe" >nul
+copy /y ".\uninstaller.cmd" "%INSTALL_DIR%\uninstaller.cmd" >nul
+
+set "REG_PATH=HKCU\Software\Microsoft\Windows\CurrentVersion\Uninstall\RDPVault"
+reg add "%REG_PATH%" /v DisplayName /d "RDP Vault" /f >nul
+reg add "%REG_PATH%" /v DisplayIcon /d "\"%INSTALL_DIR%\RDPVault.exe\",0" /f >nul
+reg add "%REG_PATH%" /v UninstallString /d "\"%INSTALL_DIR%\uninstaller.cmd\"" /f >nul
+reg add "%REG_PATH%" /v DisplayVersion /d "1.0.0" /f >nul
+reg add "%REG_PATH%" /v Publisher /d "Alon Reich" /f >nul
+
+echo Creating Desktop Shortcut...
+powershell -NoProfile -Command "$wshell = New-Object -ComObject WScript.Shell; $s = $wshell.CreateShortcut('%USERPROFILE%\Desktop\RDP Vault.lnk'); $s.TargetPath = '%INSTALL_DIR%\RDPVault.exe'; $s.WorkingDirectory = '%INSTALL_DIR%'; $s.Save()"
+
+echo.
+echo SUCCESS: RDP Vault installed and registered in appwiz.cpl.
 pause
--- uninstaller.cmd
+++ uninstaller.cmd
@@ -1,6 +1,24 @@
 @echo off
-echo Uninstalling RDP Vault...
+setlocal
+echo =========================================
+echo  UNINSTALLING RDP VAULT
+echo =========================================
+
 taskkill /F /IM RDPVault.exe /T 2>nul
-rd /s /q "%USERPROFILE%\Desktop\RDP Vault" 2>nul
-echo Uninstalled.
+set "INSTALL_DIR=%LOCALAPPDATA%\RDPVault"
+set "REG_PATH=HKCU\Software\Microsoft\Windows\CurrentVersion\Uninstall\RDPVault"
+
+echo Removing from appwiz.cpl...
+reg delete "%REG_PATH%" /f 2>nul
+
+echo Removing shortcuts...
+del /f /q "%USERPROFILE%\Desktop\RDP Vault.lnk" 2>nul
+
+echo Removing application files...
+del /f /q "%INSTALL_DIR%\RDPVault.exe" 2>nul
+:: The uninstaller script will be left behind because it is running, but we can schedule its deletion
+start /b cmd /c "timeout /t 2 >nul & rd /s /q "%INSTALL_DIR%""
+
+echo.
+echo SUCCESS: RDP Vault uninstalled.
 pause
